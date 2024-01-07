33a13c2148563ada905cd1042f9ad936b0e6608d
Amit Chaurasia <146579088+amit77t@users.noreply.github.com>
2024-01-07 12:25:54 +0530

    Add files via upload

---
+++ pythongraphics_square/main.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+
+my_pet = Turtle()
+# my_pet.shape("turtle")
+# my_pet.color("blue")
+my_pet.forward(200)
+my_pet.right(90)
+my_pet.forward(200)
+my_pet.right(90)
+my_pet.forward(200)
+my_pet.right(90)
+my_pet.forward(200)
+
+
+
+
+
+
+
+screen  =  Screen()
+screen.exitonclick()
